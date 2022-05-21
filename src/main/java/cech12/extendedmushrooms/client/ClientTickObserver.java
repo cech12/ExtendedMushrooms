bb1d3084fe4294f4e5bf1e125c09b9923b1250ce
Christian Voß <cech12@gmail.com>
2022-05-21 11:13:11 +0200

    remove buggy and unnecessary obfuscation

--- src/main/java/cech12/extendedmushrooms/client/ClientTickObserver.java
+++ src/main/java/cech12/extendedmushrooms/client/ClientTickObserver.java
@@ -6,9 +6,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-
-import java.lang.reflect.Field;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = ExtendedMushrooms.MOD_ID)
 public final class ClientTickObserver {
@@ -16,21 +13,17 @@ public final class ClientTickObserver {
     public static int ticksSinceStart = 0;
     public static float partialTicks = 0;
 
-    private static final Field RENDER_PARTIAL_TICKS_PAUSED = ObfuscationReflectionHelper.findField(Minecraft.class, "pausePartialTick");
-
     private ClientTickObserver() {}
 
     @SubscribeEvent
     public static void onRenderTick(TickEvent.RenderTickEvent event) {
-        Minecraft mc = Minecraft.getInstance();
         if (event.phase == TickEvent.Phase.START) {
+            Minecraft mc = Minecraft.getInstance();
             partialTicks = event.renderTickTime;
             if (mc.isPaused()) {
                 // If game is paused, need to use the saved value. The event is always fired with the "true" value which
                 // keeps updating when paused. See RenderTickEvent fire site for details
-                try {
-                    partialTicks = (float) RENDER_PARTIAL_TICKS_PAUSED.get(mc);
-                } catch (IllegalAccessException ignored) {}
+                partialTicks = mc.getFrameTime();
             }
         }
     }
